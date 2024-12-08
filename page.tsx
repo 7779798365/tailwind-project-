9a598ff1f24c99756d5605a5d4f49989256525c8
MUHAMMAD MOOSA <160250847+7779798365@users.noreply.github.com>
2024-12-08 20:44:08 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,27 @@
+
+import SkillsSection from "@/components/SkillSection";
+
+export default function Home() {
+  return (
+    <section>
+      <div className="grid sm:grid-cols-6 items-center ">
+        <h2 className="text-[40px] sm:col-span-2 font-heading font-[500] uppercase">
+          about me
+        </h2>
+        <div className="h-1 ml-10 max-w-[300px] sm:col-span-4 bg-gradient-to-r from-primaryColor to-secondaryColor"></div>
+      </div>
+      <p className="font-body text-[16px] mt-5 font-medium">
+        Hello there! Im thrilled to welcome you to my portfolio. I am a
+        passionate and versatile full-stack developer with a keen interest in
+        exploring the latest cutting-edge technologies. My journey in the world
+        of web development has been nothing short of exhilarating, and I
+        constantly strive to enhance my skills and embrace emerging trends in
+        the industry.
+      </p>
+      <h2 className="text-[30px] my-4 font-heading font-[500] capitalize">
+        what i do!
+      </h2>
+      <SkillsSection />
+    </section>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,41 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primaryColor: "var(--primaryColor)",
+        secondaryColor: "var(--secondaryColor)",
+      },
+      fontFamily: {
+        heading: ["var(--font-heading)"], // Poppins for headings
+        body: ["var(--font-body)"], // Raleway for body
+        logo: ["var(--font-logo)"],
+      },
+      backgroundImage: {
+        "gradient-custom": "linear-gradient(to top, #FF9C1A, #ED250A)",
+      },
+    },
+  },
+  plugins: [
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+function ({ addUtilities }: any) {   
+      addUtilities({
+        ".text-gradient": {
+          backgroundImage: "linear-gradient(to top, #FF9C1A, #ED250A)", // Gradient from orange to red
+          backgroundClip: "text", // Clips the background to the text
+          WebkitBackgroundClip: "text", // For Safari
+          color: "transparent", // Makes the text transparent
+        },
+      });
+    },
+  ],
+};
+export default config;
